c71381c9aa0306146b5dd791c86b2e3f623d9258
Anand Aji <anandaji1997@gmail.com>
2025-04-01 18:39:19 +1100

    cleanup

--- test_simple.py
+++
@@ -1,7 +0,0 @@
-from data_extractor import DataExtractor
-
-# Create an instance of the DataExtractor class
-extractor = DataExtractor('./data')
-
-# Run the gene coverage check
-extractor.check_gene_coverage_in_interactions() 
